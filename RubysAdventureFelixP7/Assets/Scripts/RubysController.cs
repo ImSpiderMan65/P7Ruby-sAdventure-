022d99f17f56e181abc174289f71fde3423922e1
Miguel Felix Cazarez <755699@student.dusd.net>
2023-11-02 15:26:27 -0700

    started on tile
    finished rubys controller and started on tile map

--- RubysAdventureFelixP7/Assets/Scripts/RubysController.cs
+++ RubysAdventureFelixP7/Assets/Scripts/RubysController.cs
@@ -13,8 +13,13 @@ public class RubysController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Debug.Log(horizontal);
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.1f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
