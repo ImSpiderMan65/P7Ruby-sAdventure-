61066b49882f3b42704e7c69cd75e56bf26b0a1b
Miguel Felix Cazarez <755699@student.dusd.net>
2023-11-26 21:37:56 -0800

    finished now on audio
    im done with text and now im on audio

--- RubysAdventureFelixP7/Assets/Scripts/RubysController.cs
+++ RubysAdventureFelixP7/Assets/Scripts/RubysController.cs
@@ -67,7 +67,11 @@ public class RubysController : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
             if (hit.collider != null)
             {
-                Debug.Log("Raycast has hit the object" + hit.collider.gameObject);
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.Displaydialog();
+                }
             }
         }
     }
